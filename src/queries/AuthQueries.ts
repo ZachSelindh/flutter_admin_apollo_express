dd478fa7451298a6abdf3106a50d314b6fcc8554
zachSelindh <zachselindh@gmail.com>
2023-02-27 14:29:13 -0600

    DOn't pass callback to bcrypt compare

--- src/queries/AuthQueries.ts
+++ src/queries/AuthQueries.ts
@@ -6,26 +6,18 @@ class AuthQueries {
   static loginQuery = async (_, { username, password }) => {
     try {
       const user = await User.findOne({ username });
-      let tempToken;
-      await bcrypt.compare(password, user.password, (error, isMatch) => {
-        if (error) {
-          throw error;
-        }
-        if (isMatch) {
-          const token = jwt.sign(
-            {
-              username: user.username,
-              password: user.password,
-              role: user.role || 'USER',
-            },
-            process.env.JWT_SECRET
-          );
-          tempToken = token;
-        }
-      });
-      if (tempToken) {
-        console.log('HIT', tempToken);
-        return tempToken;
+      const isCorrectPassword = bcrypt.compare(password, user.password);
+      if (isCorrectPassword) {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            password: user.password,
+            role: user.role || 'USER',
+          },
+          process.env.JWT_SECRET
+        );
+        console.log('HIT', isCorrectPassword);
+        return token;
       }
       throw new Error('Failed authentication');
     } catch (err) {
